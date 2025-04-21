ff2656e57494099f3757de33a2c22a27556eec23
t_hikima <t_hikima@10.202.18.164>
2025-04-21 11:00:12 +0900

    subjectdeleteaction変更

---
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,29 @@
+package scoremanager.main;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action{
+	@Override
+	public void execute(HttpServletRequest req,HttpServletResponse res)throws Exception{
+		HttpSession session=req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		SubjectDao subjectDao=new SubjectDao();
+
+		List<Subject> list = subjectDao.filter(teacher.getSchool());
+
+		req.setAttribute("subject", list);
+
+
+
+	}
+}
