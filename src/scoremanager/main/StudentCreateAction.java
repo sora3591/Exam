f583fdb75c6408f94296ff31c92691760bda52b4
stockuser <tky2402128@stu.o-hara.ac.jp>
2025-05-02 12:14:29 +0900

    細かい修正

--- src/scoremanager/main/StudentCreateAction.java
+++ src/scoremanager/main/StudentCreateAction.java
@@ -12,34 +12,45 @@ import bean.Teacher;
 import dao.ClassNumDao;
 import tool.Action;
 
-public class StudentCreateAction extends Action{
-	@Override
-	public void execute(HttpServletRequest req,HttpServletResponse res)throws Exception{
-		HttpSession session=req.getSession();
-		Teacher teacher=(Teacher)session.getAttribute("user");
+public class StudentCreateAction extends Action {
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
 
+        // クラス番号の取得
+        ClassNumDao cNumDao = new ClassNumDao();
+        List<String> classNumList = cNumDao.filter(teacher.getSchool());
 
-		ClassNumDao cNumDao=new ClassNumDao();//クラス番号Daoを初期化
-		LocalDate todaysDate=LocalDate.now();//LocalDateインスタンスを取得
-		int year=todaysDate.getYear();//現在の年を取得
+        // 入学年度の候補リスト生成（過去10年～未来9年）
+        int currentYear = LocalDate.now().getYear();
+        List<Integer> entYearSet = new ArrayList<>();
+        for (int i = currentYear - 10; i < currentYear + 10; i++) {
+            entYearSet.add(i);
+        }
 
+        // セッションからフォームの入力値とエラーを取得
+        if (session.getAttribute("errors") != null) {
+            req.setAttribute("errors", session.getAttribute("errors"));
+            session.removeAttribute("errors");
+        }
 
+        if (session.getAttribute("form_no") != null) {
+            req.setAttribute("no", session.getAttribute("form_no"));
+            req.setAttribute("name", session.getAttribute("form_name"));
+            req.setAttribute("class_num", session.getAttribute("form_classNum"));
+            req.setAttribute("ent_year", session.getAttribute("form_entYear"));
+            session.removeAttribute("form_no");
+            session.removeAttribute("form_name");
+            session.removeAttribute("form_classNum");
+            session.removeAttribute("form_entYear");
+        }
 
-		//リストを初期化
-		List<Integer>entYearSet = new ArrayList<>();
-		//１０年前から１年後まで年をリストに追加
-		for(int i=year-10; i<year+10;i++){
-			entYearSet.add(i);
-		}
-		//DBからデータを取得３
-		//ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> list=cNumDao.filter(teacher.getSchool());
+        // フォワード用のリストをセット
+        req.setAttribute("class_num_set", classNumList);
+        req.setAttribute("ent_year_set", entYearSet);
 
-
-		//リクエストにデータをセット
-				req.setAttribute("class_num_set",list);
-				req.setAttribute("ent_year_set", entYearSet);
-				//JSPへフォワード７
-				req.getRequestDispatcher("student_create.jsp").forward(req, res);
-	}
+        // JSPへフォワード
+        req.getRequestDispatcher("student_create.jsp").forward(req, res);
+    }
 }
