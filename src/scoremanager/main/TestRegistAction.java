4b5659ec7bcad7b38c31c778889a92941f5a3905
stockuser <tky2402128@stu.o-hara.ac.jp>
2025-05-08 10:40:21 +0900

    不正解

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -1,6 +1,4 @@
-
 package scoremanager.main;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,94 +16,59 @@ import dao.SubjectDao;
 import dao.TestDao;
 import tool.Action;
 
-public class TestRegistAction extends Action{
+public class TestRegistAction extends Action {
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
         HttpSession session = req.getSession();
         Teacher teacher = (Teacher) session.getAttribute("user");
 
-
-		String entYearStr="";//入力された入学年度
-		String classNum="";//入力されたクラス番号
-		LocalDate todaysDate=LocalDate.now();//LocalDateインスタンスを取得
-		int year=todaysDate.getYear();//現在の年を取得
-		ClassNumDao cNumDao=new ClassNumDao();//クラス番号Daoを初期化
+        String classNum = "";
+        LocalDate todaysDate = LocalDate.now();
+        int year = todaysDate.getYear();
+        ClassNumDao cNumDao = new ClassNumDao();
         SubjectDao subjectDao = new SubjectDao();
-        int entYear=0;//入学年度
-        int times = 0; 		//回数
-        TestDao testDao = new TestDao();	//成績情報
+        int entYear = 0;
+        int times = 0;
+        TestDao testDao = new TestDao();
         StudentDao studentDao = new StudentDao();
 
+        List<Integer> entYearSet = new ArrayList<>();
+        for (int i = year - 10; i < year + 1; i++) {
+            entYearSet.add(i);
+        }
 
+        List<String> classNumList = cNumDao.filter(teacher.getSchool());
+        List<Subject> subjectList = subjectDao.filter(teacher.getSchool());
+        List<String> subjectNames = new ArrayList<>();
 
+        for (Subject subject : subjectList) {
+            subjectNames.add(subject.getName());
+        }
 
+        req.setAttribute("subjectname", subjectNames);
+        req.setAttribute("ent_year_set", entYearSet);
+        req.setAttribute("class_num_set", classNumList);
 
+        if (req.getParameter("f1") != null) {
+            entYear = Integer.parseInt(req.getParameter("f1"));
+        }
+        if (req.getParameter("f4") != null) {
+            times = Integer.parseInt(req.getParameter("f4"));
+        }
 
+        req.setAttribute("f1", entYear);
+        req.setAttribute("f2", classNum);
+        req.setAttribute("f3", subjectNames);
+        req.setAttribute("f4", times);
 
+        if (req.getParameter("f1") != null) {
+            List<Student> students = null;
+            boolean isAttend = true;
+            int entYear2 = Integer.parseInt(req.getParameter("f1"));
+            String classNum2 = req.getParameter("f2");
+            students = studentDao.filter(teacher.getSchool(), entYear2, classNum2, isAttend);
+            req.setAttribute("student", students);
+        }
 
-		List<Integer>entYearSet = new ArrayList<>();
-		//１０年前から１年後まで年をリストに追加
-		for(int i=year-10; i<year+1;i++){
-			entYearSet.add(i);
-		}
-
-		//ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> list=cNumDao.filter(teacher.getSchool());
-
-
-		List<Subject> subjectList = subjectDao.filter(teacher.getSchool());
-
-		// 科目名だけを取り出す（必要なら）
-		List<String> subjectNames = new ArrayList<>();
-
-		for (Subject subject : subjectList) {
-		    subjectNames.add(subject.getName());
-		}
-
-
-
-
-
-
-		req.setAttribute("subjectname",subjectNames);
-		req.setAttribute("times", times);
-		req.setAttribute("ent_year_set", entYearSet);
-		req.setAttribute("class_num_set",list);
-
-
-
-
-
-		//リクエストに入学年度をセット
-		req.setAttribute("f1",entYear);
-		//リクエストにクラス番号をセット
-		req.setAttribute("f2",classNum);
-
-		req.setAttribute("f3", subjectNames);
-
-		req.setAttribute("f4", times);
-
-		if(req.getParameter("f1")!=null){
-		List<Student>students=null;
-
-		boolean isAttend= true;
-
-		int entYear2 = Integer.parseInt(req.getParameter("f1"));
-
-		String classNum2=req.getParameter("f2");
-
-
-
-		students=studentDao.filter(teacher.getSchool(), entYear2, classNum2, isAttend);
-
-		req.setAttribute("student",students);
-		}
-
-
-		req.getRequestDispatcher("test_regist.jsp").forward(req, res);
-
-
-
-
-
-}
+        req.getRequestDispatcher("test_regist.jsp").forward(req, res);
+    }
 }
