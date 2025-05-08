1d12fb0c83729648f4254f897e49b85d9027b026
s_kojima <s_kojima@10.202.17.222>
2025-05-08 11:30:40 +0900

    a

---
+++ src/scoremanager/main/TestListStudentExecuteAction.java
@@ -0,0 +1,15 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class TestListStudentExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		
+	}
+
+
+}
