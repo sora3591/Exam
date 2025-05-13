45a29419de1fc32ef362c052c7e25a45882c6a46
s_tanaka <s_tanaka@10.202.20.25>
2025-05-13 10:55:03 +0900

    変更のjavaファイル

---
+++ src/scoremanager/main/ClassUpdateAction.java
@@ -0,0 +1,23 @@
+package scoremanager.main;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class ClassUpdateAction extends Action{
+    @Override
+	public void execute(HttpServletRequest req,HttpServletResponse res)throws Exception{
+		HttpSession session=req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+
+
+		req.getRequestDispatcher("class_update.jsp").forward(req, res);
+
+
+}
+}
